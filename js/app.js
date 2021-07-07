0b78f8399122572cbc90beefad1641bc2f904df1
SandarvaShakya <sandarvashakya34@gmail.com>
2021-07-07 15:18:41 +0545

    Added form functionality, fixed img-btn size in mobile view, added scroll effect, added hover effect in card

--- js/app.js
+++ js/app.js
@@ -6,6 +6,39 @@ hamburger.addEventListener("click", () => {
 });
 
 const form = document.getElementById("contact");
+const clientName = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const msg = document.getElementById("message");
+const overlay = document.querySelector(".overlay");
+const cross = document.querySelector(".fa-times");
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  if (!clientName.value) {
+    clientName.nextElementSibling.style.display = "inline-block";
+  }
+  if (!email.value) {
+    email.nextElementSibling.style.display = "inline-block";
+  }
+  if (!number.value) {
+    number.nextElementSibling.style.display = "inline-block";
+  } else {
+    overlay.style.display = "flex";
+  }
+
+  setTimeout(function () {
+    clientName.nextElementSibling.style.display = "none";
+    email.nextElementSibling.style.display = "none";
+    number.nextElementSibling.style.display = "none";
+  }, 3000);
+});
+
+cross.addEventListener("click", function () {
+  console.log("Hello");
+  overlay.style.display = "none";
+  clientName.value = "";
+  email.value = "";
+  number.value = "";
+  msg.value = "";
 });
