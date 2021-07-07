4eec663276faf4adacf947da2926c9adfcaed85f
SandarvaShakya <sandarvashakya34@gmail.com>
2021-07-07 16:28:54 +0545

    Added image overlay function

--- js/app.js
+++ js/app.js
@@ -1,44 +1,94 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav__bottom");
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("nav__bottom--toggle");
-});
-
+// form
 const form = document.getElementById("contact");
 const clientName = document.getElementById("name");
 const email = document.getElementById("email");
 const number = document.getElementById("number");
 const msg = document.getElementById("message");
 const overlay = document.querySelector(".overlay");
-const cross = document.querySelector(".fa-times");
+const crossForm = document.querySelector(".cross-form");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (!clientName.value) {
-    clientName.nextElementSibling.style.display = "inline-block";
-  }
-  if (!email.value) {
-    email.nextElementSibling.style.display = "inline-block";
-  }
-  if (!number.value) {
-    number.nextElementSibling.style.display = "inline-block";
-  } else {
-    overlay.style.display = "flex";
-  }
+// image
+const overlayImg = document.querySelector(".overlay--img");
+const crossImg = document.querySelector(".cross-img");
+const btnImg = document.querySelectorAll(".btn--img");
 
-  setTimeout(function () {
-    clientName.nextElementSibling.style.display = "none";
-    email.nextElementSibling.style.display = "none";
-    number.nextElementSibling.style.display = "none";
-  }, 3000);
-});
+function toggleNav() {
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav__bottom--toggle");
+  });
+}
 
-cross.addEventListener("click", function () {
-  console.log("Hello");
-  overlay.style.display = "none";
-  clientName.value = "";
-  email.value = "";
-  number.value = "";
-  msg.value = "";
+function imgOverlay() {
+  btnImg.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      imgsrc = e.target.parentElement.previousElementSibling.src;
+      overlayImg.children[1].children[0].src = imgsrc;
+      overlayImg.style.display = "flex";
+      disableScroll();
+    });
+  });
+}
+
+function formSubmit() {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (!clientName.value) {
+      clientName.nextElementSibling.style.display = "inline-block";
+    }
+    if (!email.value) {
+      email.nextElementSibling.style.display = "inline-block";
+    }
+    if (!number.value) {
+      number.nextElementSibling.style.display = "inline-block";
+    } else {
+      overlay.style.display = "flex";
+      disableScroll();
+    }
+
+    setTimeout(function () {
+      clientName.nextElementSibling.style.display = "none";
+      email.nextElementSibling.style.display = "none";
+      number.nextElementSibling.style.display = "none";
+    }, 3000);
+  });
+}
+
+function disableOverlayForm() {
+  crossForm.addEventListener("click", function () {
+    overlay.style.display = "none";
+    clientName.value = "";
+    email.value = "";
+    number.value = "";
+    msg.value = "";
+    enableScroll();
+  });
+}
+
+function disableOverlayImg() {
+  crossImg.addEventListener("click", function () {
+    overlayImg.style.display = "none";
+    enableScroll();
+  });
+}
+
+function enableScroll() {
+  document.body.style.overflow = "visible";
+}
+
+function disableScroll() {
+  document.body.style.overflow = "hidden";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  toggleNav();
+  formSubmit();
+  disableOverlayForm();
+  disableOverlayImg();
+  imgOverlay();
 });
